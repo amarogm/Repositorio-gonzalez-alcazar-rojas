6136f2dee1736d3e90f194ea8132b8fcd1f7aab2
amarogm <amarogon17@gmail.com>
2025-12-08 16:05:21 -0300

    Entrega 5

---
+++ docs/script.js
@@ -0,0 +1,4 @@
+// Prueba de JS: al hacer clic en el botón aparece un mensaje
+document.getElementById("btnTest").addEventListener("click", function() {
+    alert("¡JavaScript está funcionando correctamente!");
+});
